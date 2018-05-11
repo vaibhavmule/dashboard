ae9e674c762a333ebe8cc95452cf522cecf0e71a
Joseph Mancuso <idmann509@gmail.com>
2018-05-11 16:27:51 -0400

    added single model

--- dashboard/controllers/DashboardController.py
+++ dashboard/controllers/DashboardController.py
@@ -1,6 +1,12 @@
 ''' Welcome The User To Masonite '''
+from app.User import User
 
 class DashboardController:
 
     def show(self):
         return view('/dashboard/templates/index')
+    
+    def single(self):
+        model = User.find(request().param('id'))
+        print(model)
+        return view('/dashboard/templates/single', {'model': model})
